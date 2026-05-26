0724fa11ec66ca8a53706aacef614c215c92eeb3
novemus <sergey-nine@yandex.ru>
2026-05-26 21:30:01 +0300

    package build fix

--- src/agent.h
+++ src/agent.h
@@ -3,6 +3,7 @@
 #include <boost/asio.hpp>
 #include <boost/asio/spawn.hpp>
 #include <filesystem>
+#include "export.h"
 #include "proto.h"
 
 namespace ricochet {
@@ -14,6 +15,7 @@ struct agent
     virtual void launch_relay(boost::asio::yield_context yield, const peer& red, const peer& blue) = 0;
 };
 
+LIBRICOCHET_EXPORT
 std::shared_ptr<agent> create_agent(const boost::asio::ip::tcp::endpoint& server,
                                     const std::filesystem::path& cert,
                                     const std::filesystem::path& key,
